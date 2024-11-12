0b7dc318ba670758e0a7c7548a888df86ef1f2f7
jocelyn-gonz <jg983@njit.edu>
2024-11-12 11:41:12 -0500

    class work

---
+++ intro.js
@@ -0,0 +1,54 @@
+var age = 20;
+var age;
+age=100;
+console.log(age);
+let rollNo = 21; 
+console.log(rollNo)
+
+const valuePi=3.14;
+console.log(valuePi)
+
+
+
+var height = 160
+var height;
+console.log(height)
+
+let userName= "Harry";
+
+console.log(userName);
+
+let pet = true;
+
+console.log(pet);
+
+let house= null;
+
+console.log(house);
+
+let bigNumber =2459345874398570439580230938509328328598325888393483248932895n;
+
+console.log(bigNumber);
+
+let firstname="Jocelyn"
+
+let lastname= "Gonzalez"
+console.log(firstname,lastname);
+
+let a = 100;
+let b= '100';
+//a +=10; ==> a= a+10
+a+=10;
+a*=10;
+console.log(a+b);
+console.log(a !== b);
+//console.log(a!=b);
+
+let c = true;
+let d= false;
+console.log(c&&d);
+
+if(firstname== "Jocelyn"){ 
+    house="Gryffindor";
+    console.log(house);
+}
